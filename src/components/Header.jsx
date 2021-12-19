203206c1a5d7ca09f9190217a2af9a45bb36ffb2
Franco <francomorales145@gmail.com>
2021-12-19 18:05:42 -0300

    Pág. Home provisoria

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
 
+import  "../scss/header.scss";
+
 function Header() {
     return ( 
-        <>
-            <p>¡Header Work!</p>
-        </>
+        <header id="header-home">
+            <div className='wrap-content'>
+                <div className='overlay-text bg-translucid text-light border border-light'>
+                    <h1>The Garden</h1>
+                    <hr />
+                    <p>Best E-Commerce for buy indoors plants</p>
+                </div>
+            </div>
+        </header>
     );
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,58 @@
 import React from 'react';
 
+import "../scss/home.scss";
 import Header from "../components/Header";
+
+import CualityIcon from "../assets/icons/comercio.png";
+import LocalizationIcon from "../assets/icons/localizacion.png";
+import SecureIcon from "../assets/icons/seguro.png";
 
 
 function Home() {
     return (  
         <>
             <Header />
-            <div className='container'>
-            </div>
+            <main id="landing">
+                <div className='container'>
+                    <section id="ecommerce-desc">
+                        <div className='row'>
+                            <div className='col-12 col-md-6 col-lg-4'>
+                                <div className="card shadow-lg bg-dark text-light">
+                                    <div className='float-icon bg-light'> 
+                                        <img src={CualityIcon} alt="cuality icon" className="icon"/>
+                                    </div>
+                                    <div className="card-body">
+                                        <h3 className="card-title mb-3">High cuality products</h3>
+                                        <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, voluptatum repudiandae, corrupti quia deserunt adipisci at quasi dolorum delectus deleniti temporibus. Quibusdam cupiditate alias aliquam? Minus laborum aperiam soluta voluptates.</p>
+                                    </div>
+                                </div>                        
+                            </div>
+                            <div className='col-12 col-md-6 col-lg-4'>
+                                <div className="card shadow-lg bg-dark text-light">
+                                    <div className='float-icon bg-light'> 
+                                        <img src={LocalizationIcon} alt="world icon" className="icon"/>
+                                    </div>
+                                    <div className="card-body">
+                                        <h3 className="card-title mb-3">Worldwide shipping</h3>
+                                        <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, voluptatum repudiandae, corrupti quia deserunt adipisci at quasi dolorum delectus deleniti temporibus. Quibusdam cupiditate alias aliquam? Minus laborum aperiam soluta voluptates.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className='col-12 col-md-6 col-lg-4'>
+                                <div className="card shadow-lg bg-dark text-light">
+                                    <div className='float-icon bg-light'> 
+                                        <img src={SecureIcon} alt="secure icon" className="icon"/>
+                                    </div>
+                                    <div className="card-body">
+                                        <h3 className="card-title mb-3">Secure first</h3>
+                                        <p className="card-text">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quas, voluptatum repudiandae, corrupti quia deserunt adipisci at quasi dolorum delectus deleniti temporibus. Quibusdam cupiditate alias aliquam? Minus laborum aperiam soluta voluptates.</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </section>
+                </div>
+            </main>
         </>
     );
 }
